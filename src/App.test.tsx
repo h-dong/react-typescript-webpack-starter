c5fc13700906ee1e34e0ea7c87fbdf18dc18f969
Hao Dong <dong_hao@hotmail.co.uk>
2018-06-18 16:37:51 +0100

    Rename back to desired file name

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,6 @@
 import { shallow } from 'enzyme';
 import React from 'react';
-import App from './App2';
+import App from './App';
 
 describe('Test', () => {
     it('renders three <Foo /> components', () => {
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import App from './App2';
+import App from './App';
 
 render(
     <App message="World" />,
